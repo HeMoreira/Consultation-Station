7790e0ea4e3970fb79cfb33bc548fde8c51fc795
Henrique <henriquefmoreira2@gmail.com>
2025-09-19 20:03:38 -0300

    style: formulário de consultas e link buttons

--- ConsultationStation/consultations/forms.py
+++ ConsultationStation/consultations/forms.py
@@ -3,9 +3,9 @@ from .models import Consultation
 
 # Formulário para criação de novas consultas
 class ConsultationForm(forms.ModelForm):
-    patient_name = forms.CharField(label="Nome do Paciente", max_length=100, widget=forms.TextInput(attrs={'size': 50}))
-    date = forms.DateTimeField(label="Horário da Consulta", widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
-    description = forms.CharField(label="", max_length=200, widget=forms.Textarea(attrs={'placeholder': 'Descreva o motivo da consulta', 'size': 60, 'rows': 4}))
+    patient_name = forms.CharField(label="Nome do Paciente", max_length=100, widget=forms.TextInput(attrs={'size': 42}))
+    date = forms.DateTimeField(label="Horário", widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
+    description = forms.CharField(label="", max_length=200, widget=forms.Textarea(attrs={'placeholder': 'Descreva o motivo da consulta', 'rows': 4}))
     duration = forms.IntegerField(label="Duração (minutos)", initial=30, min_value=30, max_value=120)
 
     class Meta:
