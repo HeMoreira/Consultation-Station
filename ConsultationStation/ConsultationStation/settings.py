e153f2c7778b16ad2351e280678594f5148d8e6c
Henrique <henriquefmoreira2@gmail.com>
2025-10-15 14:08:23 -0300

    Preparação para deploy no render

--- ConsultationStation/ConsultationStation/settings.py
+++ ConsultationStation/ConsultationStation/settings.py
@@ -127,6 +127,13 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
